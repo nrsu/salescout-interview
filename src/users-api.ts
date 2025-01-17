3cc83f9d62f2ad0653e21905b04cfcc38c1ebcf7
bekzhan <75430472+nrsu@users.noreply.github.com>
2025-01-17 16:40:40 +0500

    Update users-api.ts

--- src/users-api.ts
+++ src/users-api.ts
@@ -12,7 +12,12 @@ app.use(express.json());
 const users: { name: string }[] = [];
 
 app.post('/user', (req: Request, res: Response) => {
-    res.status(200).send();
+    const {name} = req.body;
+    if(!name){
+        res.status(400).json({error:'Name is required'})
+    }
+    users.push({ name });
+    res.status(200).json({ message: 'User added', user: { name } });
 });
 
 app.get('/users', (req: Request, res: Response) => {
