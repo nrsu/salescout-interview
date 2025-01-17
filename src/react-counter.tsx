62b46343e671dab804d633b903259b8691038b99
bekzhan <75430472+nrsu@users.noreply.github.com>
2025-01-17 16:40:05 +0500

    Update react-counter.tsx

--- src/react-counter.tsx
+++ src/react-counter.tsx
@@ -1,10 +1,16 @@
 // Implement a Counter component with two buttons:
 // “Increase” and “Decrease”, which displays the current counter value.
-import React from 'react'
+import React, { useState } from 'react';
 
 function Counter() {
-    // Your code goes here
-    return (<></>)
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={() => setCount(count+1)}>Increase</button>
+            <button onClick={() => setCount(count-1)}>Decrease</button>
+        </div>
+    );
 }
 
-export default Counter
+export default Counter;
